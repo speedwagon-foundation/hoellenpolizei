11046a3f75dfe0fb518a14ccbc511a79f82ac07b
lhofwimmer <lukas.hofwimmer@gmail.com>
2018-12-12 11:38:44 +0100

    ADD load api token from json file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,24 @@
-
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.Klaxon
+import com.beust.klaxon.Parser
+import java.io.File
 
 fun main() {
+    val token = loadCredentials()
+    if (token == null) println("--Error: failed to load credentials.")
 }
 
-fun loadCredentials() {
+fun loadCredentials(): String? {
+    val fileName = "credentials.json"
+    return (parse(fileName) as JsonObject).string("token")
+}
+
+fun parse(fileName: String): Any? {
+    val resourceDir = System.getProperty("user.dir")
+    val fullPath = """$resourceDir\src\main\resources\$fileName"""
+    return File(fullPath).let {file ->
+        file.reader().use {
+            Parser().parse(it)
+        }
+    }
 }
