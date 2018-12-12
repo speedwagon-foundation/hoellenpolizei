0b9b451ea865c04d7cd5346dcc09e6939f635f27
Andreas Zauner <andi4971@gmail.com>
2018-12-12 11:50:14 +0100

    ADD example message answer

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,13 @@
-
+import org.javacord.api.DiscordApiBuilder
 
 fun main() {
+    var token = loadCredentials()
+    var api =  DiscordApiBuilder().setToken(token).login().join()
+    api.addMessageCreateListener {
+        it.messageContent.startsWith("!ping")
+        it.channel.sendMessage("hello")
+    }
 }
 
-fun loadCredentials() {
+fun loadCredentials() :String {
 }
