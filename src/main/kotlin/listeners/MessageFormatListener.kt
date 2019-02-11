a4c0410832ce17c86769cb6aa6030deef5926807
Lukas Hofwimmer <lukas.hofwimmer@gmail.com>
2019-02-11 20:21:28 +0100

    MOD moved to new json file structure

--- src/main/kotlin/listeners/MessageFormatListener.kt
+++ src/main/kotlin/listeners/MessageFormatListener.kt
@@ -6,13 +6,15 @@ import org.javacord.api.event.message.MessageCreateEvent
 class MessageFormatListener : BaseListener() {
     override fun onMessageCreate(event: MessageCreateEvent?) {
         if(super.checkIfAuthorIsBot(event!!)) return
-        if (ConfigManager.instance.markupChannels.contains(event?.message?.channel?.id)) {
-            if (!event?.messageAttachments?.isNullOrEmpty()!!) return
+
+        if (ConfigManager.instance.markupChannels.contains(event.message?.channel?.id)) {
+            if (!event.messageAttachments?.isNullOrEmpty()!!) return
+
             if (event.messageContent.length < 2000) {
                 var lang = event.messageContent.takeWhile { it != ' ' }
-                var content: String
-                if (ConfigManager.instance.allowedLanguages.containsKey(lang)) {
-                    lang = ConfigManager.instance.allowedLanguages[lang]!!
+                val content: String
+                if (ConfigManager.instance.allowedLanguages.filter{ it.highlightjs == lang }.count() > 0) {
+                    lang = ConfigManager.instance.allowedLanguages.first { it.fileType == lang }.fileType
                     content = event.messageContent.substring(lang.length+1)
                 }else {
                     lang = "ts"
